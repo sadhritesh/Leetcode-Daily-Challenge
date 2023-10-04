7bb9b00e6aace61c19c9778b0f5c352af2366f7e
Ritesh Sadh <sadhritesh32@gmail.com>
2023-10-04 10:09:30 +0530

    October-Day4

---
+++ October-Challenge/Day4.py
@@ -0,0 +1,75 @@
+"""
+706. Design HashMap 
+
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+ 
+
+Example 1:
+
+Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+Explanation
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+"""
+class MyHashMap:
+
+    def __init__(self):
+        self.map = []
+        
+    def put(self, key: int, value: int) -> None:
+        map = self.map
+        size = len(map)
+        for i in range(size):
+            if map[i][0] == key:
+                map[i][1] = value
+                break 
+        else:
+            temp = []
+            temp.append(key)
+            temp.append(value)
+            map.append(temp)
+    
+
+    def get(self, key: int) -> int:
+        map = self.map 
+        size = len(map)
+        for i in range(size):
+            if map[i][0] == key:
+                return map[i][1]
+        return -1
+
+    def remove(self, key: int) -> None:
+        map = self.map 
+        size = len(map)
+        for i in range(size):
+            if map[i][0] == key:
+                temp = [map[i][0],map[i][1]]
+                map.remove(temp)
+                break 
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key) 
